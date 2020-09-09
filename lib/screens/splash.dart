07de73861c7edac97bc41f170316d1a35a3b7206
Abubakar Abdusalam <bynalabs@gmail.com>
2020-09-09 21:15:32 +0100

    added splashscreen timer

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,30 @@
+import 'package:aad_practice_project/screens/screen_one.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static const String id = 'splash';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<bool> _splash() async {
+    await Future.delayed(Duration(milliseconds: 5000), () {});
+
+    return true;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _splash().then((status) {
+      if (status) {
+        Navigator.pushReplacementNamed(context, ScreenOne.id);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
